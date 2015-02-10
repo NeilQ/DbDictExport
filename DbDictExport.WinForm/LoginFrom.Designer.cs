dc189a41f240b6b68659e7eafec29f49a614f2dd
NeilQ <neil_chen_0918@live.cn>
2015-02-10 19:49:47 +0800

    change login form ux

--- DbDictExport.WinForm/LoginFrom.Designer.cs
+++ DbDictExport.WinForm/LoginFrom.Designer.cs
@@ -45,9 +45,9 @@
             // lblServerType
             // 
             this.lblServerType.AutoSize = true;
-            this.lblServerType.Location = new System.Drawing.Point(42, 31);
+            this.lblServerType.Location = new System.Drawing.Point(37, 31);
             this.lblServerType.Name = "lblServerType";
-            this.lblServerType.Size = new System.Drawing.Size(58, 20);
+            this.lblServerType.Size = new System.Drawing.Size(54, 20);
             this.lblServerType.TabIndex = 0;
             this.lblServerType.Text = "Engine";
             // 
@@ -57,16 +57,16 @@
             this.cmbServerType.FormattingEnabled = true;
             this.cmbServerType.Items.AddRange(new object[] {
             "Database Engine"});
-            this.cmbServerType.Location = new System.Drawing.Point(165, 28);
+            this.cmbServerType.Location = new System.Drawing.Point(147, 28);
             this.cmbServerType.Name = "cmbServerType";
-            this.cmbServerType.Size = new System.Drawing.Size(248, 28);
+            this.cmbServerType.Size = new System.Drawing.Size(221, 28);
             this.cmbServerType.TabIndex = 1;
             // 
             // lblServer
             // 
             this.lblServer.AutoSize = true;
-            this.lblServer.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lblServer.Location = new System.Drawing.Point(42, 65);
+            this.lblServer.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblServer.Location = new System.Drawing.Point(37, 65);
             this.lblServer.Name = "lblServer";
             this.lblServer.Size = new System.Drawing.Size(56, 20);
             this.lblServer.TabIndex = 2;
@@ -78,9 +78,9 @@
             this.cmbServer.Items.AddRange(new object[] {
             ".",
             "(local)"});
-            this.cmbServer.Location = new System.Drawing.Point(165, 62);
+            this.cmbServer.Location = new System.Drawing.Point(147, 62);
             this.cmbServer.Name = "cmbServer";
-            this.cmbServer.Size = new System.Drawing.Size(248, 28);
+            this.cmbServer.Size = new System.Drawing.Size(221, 28);
             this.cmbServer.TabIndex = 3;
             // 
             // cmbAuthentication
@@ -89,60 +89,60 @@
             this.cmbAuthentication.Items.AddRange(new object[] {
             "Sql Server Authentication",
             "Windows Authentication"});
-            this.cmbAuthentication.Location = new System.Drawing.Point(165, 97);
+            this.cmbAuthentication.Location = new System.Drawing.Point(147, 97);
             this.cmbAuthentication.Name = "cmbAuthentication";
-            this.cmbAuthentication.Size = new System.Drawing.Size(248, 28);
+            this.cmbAuthentication.Size = new System.Drawing.Size(221, 28);
             this.cmbAuthentication.TabIndex = 4;
             // 
             // lblAuthentication
             // 
             this.lblAuthentication.AutoSize = true;
-            this.lblAuthentication.Location = new System.Drawing.Point(42, 100);
+            this.lblAuthentication.Location = new System.Drawing.Point(37, 100);
             this.lblAuthentication.Name = "lblAuthentication";
-            this.lblAuthentication.Size = new System.Drawing.Size(117, 20);
+            this.lblAuthentication.Size = new System.Drawing.Size(106, 20);
             this.lblAuthentication.TabIndex = 5;
             this.lblAuthentication.Text = "Authentication";
             // 
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(81, 153);
+            this.lblUsername.Location = new System.Drawing.Point(72, 153);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(82, 20);
+            this.lblUsername.Size = new System.Drawing.Size(75, 20);
             this.lblUsername.TabIndex = 6;
             this.lblUsername.Text = "Username";
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(81, 186);
+            this.lblPassword.Location = new System.Drawing.Point(72, 186);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(78, 20);
+            this.lblPassword.Size = new System.Drawing.Size(71, 20);
             this.lblPassword.TabIndex = 7;
             this.lblPassword.Text = "Password";
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(165, 150);
+            this.txtUsername.Location = new System.Drawing.Point(147, 150);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(188, 27);
+            this.txtUsername.Size = new System.Drawing.Size(168, 27);
             this.txtUsername.TabIndex = 8;
             this.txtUsername.Text = "sa";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(165, 183);
+            this.txtPassword.Location = new System.Drawing.Point(147, 183);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(188, 27);
+            this.txtPassword.Size = new System.Drawing.Size(168, 27);
             this.txtPassword.TabIndex = 9;
             this.txtPassword.UseSystemPasswordChar = true;
             // 
             // btnConnect
             // 
             this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnConnect.Location = new System.Drawing.Point(165, 228);
+            this.btnConnect.Location = new System.Drawing.Point(147, 228);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(115, 35);
+            this.btnConnect.Size = new System.Drawing.Size(102, 35);
             this.btnConnect.TabIndex = 11;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -151,9 +151,9 @@
             // btnCancel
             // 
             this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCancel.Location = new System.Drawing.Point(309, 228);
+            this.btnCancel.Location = new System.Drawing.Point(275, 228);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(104, 35);
+            this.btnCancel.Size = new System.Drawing.Size(92, 35);
             this.btnCancel.TabIndex = 12;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -161,10 +161,11 @@
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(462, 285);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(411, 285);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.txtPassword);
@@ -177,7 +178,7 @@
             this.Controls.Add(this.lblServer);
             this.Controls.Add(this.cmbServerType);
             this.Controls.Add(this.lblServerType);
-            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "LoginForm";
