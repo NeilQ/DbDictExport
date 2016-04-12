7244eb82d436b64ddc813645f02645e31a95d6b1
Chen <neil_chen_0918@live.cn>
2016-04-12 17:01:34 +0800

    treeview demo

---
+++ DbDictExport.Model/DbCategory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace DbDictExport.Core
+{
+    public class DbCategory
+    {
+        public string Name { get; set; }
+
+        public List<DbColumn> Columns { get; set; } = new List<DbColumn>()
+        {
+            new DbColumn() {Name = "table1"},
+            new DbColumn() {Name = "table2"}
+        };
+    }
+}
---
+++ DbDictExport.WPF/Pages/HomeViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Data.SqlClient;
+using DbDictExport.Core;
+using DbDictExport.Core.Dal;
+
+namespace DbDictExport.WPF.Pages
+{
+    public class HomeViewModel : INotifyPropertyChanged, IDataErrorInfo
+    {
+        private readonly SqlConnectionStringBuilder _connStringBuilder;
+
+        public List<DbCategory> DbCategories { get; set; } = new List<DbCategory>();
+        public HomeViewModel()
+        {
+            _connStringBuilder = Global.ConnectionBuilder as SqlConnectionStringBuilder;
+
+            if (_connStringBuilder != null)
+            {
+                var dbNames = DataAccess.GetDbNameList(_connStringBuilder);
+                if (dbNames != null)
+                {
+                    foreach (var dbName in dbNames)
+                    {
+                        DbCategories.Add(new DbCategory()
+                        {
+                            Name = dbName
+                        });
+                    }
+                }
+            }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Error
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
